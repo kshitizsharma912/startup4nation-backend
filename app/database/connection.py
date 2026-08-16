863a0534599d72c837bb56d41f5f2ba43aac10e9
Kshitizsharma912 <kshitizsharma83755@gmail.com>
2026-08-17 01:13:48 +0530

    Fix database environment configuration

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:1092@localhost:5432/startup4nation"
+import os
+DATABASE_URL =os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
